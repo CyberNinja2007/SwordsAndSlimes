f954ed4b0a1c1b8274b541a2eac1a26cac788a14
Georgy <cyberninja2019@protonmail.com>
2022-02-13 20:45:42 +0300

    Added displaying of custom name

--- SwordsAndSlimes.WEB/Models/CharacterIndexViewModel.cs
+++ SwordsAndSlimes.WEB/Models/CharacterIndexViewModel.cs
@@ -1,11 +1,18 @@
-﻿namespace SwordsAndSlimes.WEB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SwordsAndSlimes.WEB.Models
 {
     public class CharacterIndexViewModel
     {
+        [Display(Name = "Имя")]
         public string Name { get; set; }
+        [Display(Name = "Уровень")]
         public int? Level { get; set; }
+        [Display(Name = "Здоровье")]
         public int? Health { get; set; }
+        [Display(Name = "Сила атаки")]
         public int? Attack { get; set; }
+        [Display(Name = "Класс")]
         public string Class { get; set; }
     }
 }
--- SwordsAndSlimes.WEB/Models/DungeonIndexViewModel.cs
+++ SwordsAndSlimes.WEB/Models/DungeonIndexViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace SwordsAndSlimes.WEB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SwordsAndSlimes.WEB.Models
 {
     public class DungeonIndexViewModel
     {
+        [Display(Name = "Название")]
         public string Name { get; set; }
+        [Display(Name = "Изображение")]
         public string Image { get; set; }
     }
 }
